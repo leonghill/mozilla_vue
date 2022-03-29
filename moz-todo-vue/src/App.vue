e181a4f8ac242100b8bf7a4f832fbd3b1d933220
leonghill <leong.hill.web@gmail.com>
2022-03-29 16:56:08 +0800

    Creating a New To-Do form

--- moz-todo-vue/src/App.vue
+++ moz-todo-vue/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>My To-Do List</h1>
+    <to-do-form></to-do-form> 
     <ul>
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
@@ -12,12 +13,14 @@
 
 <script>
 import ToDoItem from './components/ToDoItem.vue';
+import ToDoForm from './components/ToDoForm.vue';
 import uniqueId from 'lodash.uniqueid'
 
 export default {
   name: 'app',
   components: {
-    ToDoItem
+    ToDoItem,
+    ToDoForm,
   },
   data() {
     return {
