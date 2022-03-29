19aec3e0751817e0beafc5207226ef6aed3e5eaf
leonghill <leong.hill.web@gmail.com>
2022-03-29 22:45:41 +0800

    Adding a summary conter

--- moz-todo-vue/src/App.vue
+++ moz-todo-vue/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>My To-Do List</h1>
+    <h2 id="list-summary">{{listSummary}}</h2>
     <to-do-form @todo-added="addToDo"></to-do-form>
     <ul aria-labelledby="list-summary" class="stack-large">
       <li v-for="item in ToDoItems" :key="item.id">
@@ -28,16 +29,24 @@ export default {
         { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
         { id: uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true },
         { id: uniqueId('todo-'), label: 'Have fun', done: true },
-        { id: uniqueId('todo-'), label: 'Create a to-do list', done: false }
+        { id: uniqueId('todo-'), label: 'Create a to-do list', done: false },
       ]
     };
   },
   methods: {
     addToDo(toDoLabel) {
   this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false});
+    },    
+  },
+  computed: {
+    listSummary() {
+      const numberFinishedItems = this.ToDoItems.filter(item =>item.done).length
+      return `${numberFinishedItems} out of ${this.ToDoItems.length} items completed`
     }
-  }
+  }  
 };
+
+
 </script>
 
 <style>
